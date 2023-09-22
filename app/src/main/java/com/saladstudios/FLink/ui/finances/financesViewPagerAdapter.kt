cbd0ee4b6f8a8f2eb26ed83b6c14c269c5d08e05
Sascha <tramberger.s@gmail.com>
2023-09-22 16:45:50 +0200

    Trying to fix deprecation didn't work. It seems we must use ViewPager2 in future instead.

--- app/src/main/java/com/saladstudios/FLink/ui/finances/financesViewPagerAdapter.kt
+++ app/src/main/java/com/saladstudios/FLink/ui/finances/financesViewPagerAdapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
-class financesViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm){
+class financesViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm,BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
     override fun getCount(): Int {
         return 4;
     }
