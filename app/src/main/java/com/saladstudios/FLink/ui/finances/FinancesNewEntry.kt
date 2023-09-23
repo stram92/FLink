6e1cf946a0beca401ff4e4e27cbf3977b9254bf7
Sascha <tramberger.s@gmail.com>
2023-09-23 15:23:09 +0200

    Building Finances New Entry Logic

--- app/src/main/java/com/saladstudios/FLink/ui/finances/FinancesNewEntry.kt
+++ app/src/main/java/com/saladstudios/FLink/ui/finances/FinancesNewEntry.kt
@@ -1,11 +1,15 @@
 package com.saladstudios.FLink.ui.finances
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
+import android.text.format.DateFormat
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.saladstudios.FLink.R
 import com.saladstudios.FLink.databinding.FinancesNewEntryBinding
+import java.util.*
 
 class FinancesNewEntry : AppCompatActivity() {
     private lateinit var binding: FinancesNewEntryBinding
@@ -20,7 +24,53 @@ class FinancesNewEntry : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        binding.financesNewEntrySave.setOnClickListener { newEntrySave() }
     }
 
+    fun newEntrySave() {
+        var returnIntent = Intent()
+        var payer = ""
+        var payedfor = ""
+        var payedforamount = ""
+        val dateMillis: Long = binding.financesNewEntryCalendar.date
+        val date = Date(dateMillis)
+        var entryDate = ""
 
+        if (binding.financesNewEntrySwitchSascha.isChecked) {
+            payer = "S"
+        } else if (binding.financesNewEntrySwitchDenise.isChecked) {
+            payer = "D"
+        } else {
+            payer = "B"
+        }
+
+        if (binding.financesNewEntryForSascha.isChecked && !binding.financesNewEntryForDenise.isChecked) {
+            payedfor = "S"
+        } else if (!binding.financesNewEntryForSascha.isChecked && binding.financesNewEntryForDenise.isChecked) {
+            payedfor = "D"
+        } else {
+            payedfor = "B"
+        }
+
+        if (payedfor.equals("S")) {
+            payedforamount=binding.financesNewEntryAmountSaschaInput.text.toString()
+        } else if (payedfor.equals("D")) {
+            payedforamount=binding.financesNewEntryAmountDeniseInput.text.toString()
+        }
+
+        entryDate = DateFormat.format("dd", date).toString() + "." + DateFormat.format("MM",date).toString() + "." + DateFormat.format("yyyy",date).toString()
+
+
+        returnIntent.putExtra("payer", payer)
+        returnIntent.putExtra("description", binding.financesNewEntryDescriptionInput.text.toString())
+        returnIntent.putExtra("amount", binding.financesNewEntryAmountInput.text.toString())
+        returnIntent.putExtra("entryDate", entryDate)
+        returnIntent.putExtra("payedFor", payedfor)
+        returnIntent.putExtra("payedForAmount", payedforamount)
+
+        setResult(Activity.RESULT_OK,returnIntent)
+
+        finish()
+    }
 }
--- app/src/main/java/com/saladstudios/FLink/ui/finances/FinancesOverviewFragment.kt
+++ app/src/main/java/com/saladstudios/FLink/ui/finances/FinancesOverviewFragment.kt
@@ -1,8 +1,10 @@
 package com.saladstudios.FLink.ui.finances
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,12 +17,17 @@ import com.saladstudios.FLink.databinding.FragmentFinancesOverviewBinding
 import com.saladstudios.FLink.utility.json.addJsonEntryLocal
 import com.saladstudios.FLink.utility.json.readJsonFileLocal
 import com.saladstudios.FLink.utility.json.wipeJsonEntriesLocal
+import java.time.format.DateTimeFormatter
 
 class FinancesOverviewFragment : Fragment() {
     private val viewModel: FinancesViewModel by viewModels()
 
     private lateinit var binding: FragmentFinancesOverviewBinding
     private lateinit var financesNewIntent: Intent
+
+    private var LAUNCH_NEW_ENTRY = 1
+
+    private lateinit var financesRecyclerView: RecyclerView
 
     @Override
     override fun onCreateView(
@@ -35,21 +42,24 @@ class FinancesOverviewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val financesRecyclerView = view.findViewById<RecyclerView>(R.id.financesRecyclerView)
+        financesRecyclerView = view.findViewById<RecyclerView>(R.id.financesRecyclerView)
 
         financesRecyclerView.layoutManager = LinearLayoutManager(view.context)
         financesRecyclerView.setHasFixedSize(true)
 
         val financesData = ArrayList<FinancesItemsViewModel>()
 
+
         wipeJsonEntriesLocal(view.context)
 
+        /*
         addJsonEntryLocal(view.context,"S","Kino","- 15,00 €","01.12.2023","B","Denise: - 7,50 €")
         addJsonEntryLocal(view.context,"D","Einkaufen","- 142,12 €","01.08.2023","B","Sascha: - 71,06 €")
         addJsonEntryLocal(view.context,"D","Pfeile","- 300,00 €","01.09.2023","D","")
         addJsonEntryLocal(view.context,"D","Gehalt","+ 3.000,00 €","02.09.2023","D","Denise: + 3.000 €")
         addJsonEntryLocal(view.context,"B","Kredit","- 2.000,00 €","01.09.2023","B","")
         addJsonEntryLocal(view.context,"S","Lachgummi","- 3,00 €","22.09.2023","S","")
+        */
 
         financesRecyclerView.adapter = refreshFinances (view.context)
 
@@ -81,11 +91,29 @@ class FinancesOverviewFragment : Fragment() {
     }
 
     private fun financesAdd(context: Context, financesRecyclerView: RecyclerView) {
-        /*addJsonEntryLocal(context,"S","Kino","- 15,00 €","01.12.2023","B","Denise: - 7,50 €")
-
-        financesRecyclerView.adapter = refreshFinances (context)*/
         financesNewIntent = Intent(context, FinancesNewEntry::class.java)
-        startActivity(financesNewIntent)
+        startActivityForResult(financesNewIntent,LAUNCH_NEW_ENTRY)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode==LAUNCH_NEW_ENTRY) {
+            if (resultCode== Activity.RESULT_OK){
+                    Log.d("FLinkTest","SEas")
+                    Log.d("FLinkTest",data!!.getStringExtra("payer").toString())
+                    addJsonEntryLocal(requireContext(),
+                        data!!.getStringExtra("payer").toString(),
+                        data!!.getStringExtra("description").toString(),
+                        data!!.getStringExtra("amount").toString(),
+                        data!!.getStringExtra("entryDate").toString(),
+                        data!!.getStringExtra("payedFor").toString(),
+                        data!!.getStringExtra("payedForAmount").toString()
+                    )
+
+            }
+        }
+
+        financesRecyclerView.adapter = refreshFinances(binding.root.context)
     }
 }
